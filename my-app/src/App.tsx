7505d82b36ebb72aaa7e56ea2f3048c32e7325c4
Renato Santos <devrenato.js@gmail.com>
2025-03-08 17:16:20 -0300

    feat: added more inputs

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
 
 import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
-
-const formSchema = z.object({
-  name: z.string().min(1, "Name should be longer than 1 character"),
-});
-
-type TFormValues = z.infer<typeof formSchema>;
+import {
+  formSchema,
+  onSubmitAddUser,
+  TFormValues,
+} from "./schemas/formAddUserSchema";
 
 function App() {
   const {
@@ -23,78 +20,180 @@ function App() {
 
   const watchAllFields = watch();
 
-  const onSubmit: SubmitHandler<TFormValues> = (data) => {
-    console.log("data submitted", data);
-  };
-
   return (
     <>
       <div className="grid grid-cols-2 gap-5">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="name"
-          >
-            <span>Nome (name)</span>
-            <input className="border border-black" id="name" type="text" />
-          </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="email"
-          >
-            <span>E-mail (email)</span>
-            <input className="border border-black" id="email" type="text" />
-          </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="password"
-          >
-            <span>Senha (password)</span>
-            <input className="border border-black" id="password" type="text" />
-          </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="repassword"
-          >
-            <span>Confirmar senha (repassword)</span>
+        <form onSubmit={handleSubmit(onSubmitAddUser)}>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Nome (name)</span>
             <input
+              {...register("name")}
               className="border border-black"
-              id="repassword"
+              id="name"
+              title="name"
               type="text"
             />
+            {errors.name && <p>{errors.name.message}</p>}
           </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="age"
-          >
-            <span>Idade (age) Só número:</span>
-            <input className="border border-black" id="age" type="text" />
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">E-mail (email):</span>
+            <input
+              {...register("email")}
+              className="border border-black"
+              id="email"
+              title="email"
+              type="email"
+            />
+            {errors.email && <p>{errors.email.message}</p>}
           </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="genre"
-          >
-            <select name="genre" id="genre" className="border border-black">
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Salário (salary) (máscara dinheiro)</span>
+            <input
+              {...register("salary")}
+              className="border border-black"
+              id="salary"
+              title="salary"
+              type="salary"
+            />
+            {errors.salary && <p>{errors.salary.message}</p>}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Aumento (raise) (máscara porcentagem)</span>
+            <input
+              {...register("raise")}
+              className="border border-black"
+              id="raise"
+              title="raise"
+              type="raise"
+            />
+            {errors.raise && <p>{errors.raise.message}</p>}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">CEP (cep) (máscara cep)</span>
+            <input
+              {...register("cep")}
+              className="border border-black"
+              id="cep"
+              title="cep"
+              type="cep"
+            />
+            {errors.cep && <p>{errors.cep.message}</p>}{" "}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">CPF (cpf) (máscara cpf)</span>
+            <input
+              {...register("cpf")}
+              className="border border-black"
+              id="cpf"
+              title="cpf"
+              type="cpf"
+            />
+            {errors.cpf && <p>{errors.cpf.message}</p>}{" "}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Dias trabalhados (workdays)</span>
+            <input className="border border-black" id="workdays" type="text" />
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Senha (password)</span>
+            <input
+              {...register("password")}
+              className="border border-black"
+              id="password"
+              title="password"
+              type="password"
+            />
+            {errors.password && <p>{errors.password.message}</p>}{" "}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Confirmar senha (repassword)</span>
+            <input
+              {...register("repassword")}
+              className="border border-black"
+              id="repassword"
+              title="repassword"
+              type="repassword"
+            />
+            {errors.repassword && <p>{errors.repassword.message}</p>}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Idade (age) Só número:</span>
+            <input
+              {...register("age")}
+              className="border border-black"
+              id="age"
+              title="age"
+              type="age"
+            />
+            {errors.age && <p>{errors.age.message}</p>}{" "}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">
+              Data de nascimento (birthday) (Máscara data):
+            </span>
+            <input
+              {...register("birthday")}
+              className="border border-black"
+              id="birthday"
+              title="birthday"
+              type="birthday"
+            />
+            {errors.birthday && <p>{errors.birthday.message}</p>}{" "}
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <select
+              {...register("genre")}
+              title="genre"
+              id="genre"
+              className="border border-black"
+            >
               <option value="">-- Choose --</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
             </select>
           </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="agree"
-          >
-            <span>Concordo (agree)</span>
-            <input type="checkbox" />
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Concordo (agree)</span>
+            <input {...register("agree")} title="agree" type="checkbox" />
           </label>
-          <label
-            className="block border border-black py-2 px-2 bg-gray-100"
-            htmlFor="theme"
-          >
-            <input type="radio" name="theme" />
-            <span>Preferência Dark (theme)</span>
-            <input type="radio" name="theme" />
-            <span>Preferência Light (theme)</span>
+
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <input
+              type="radio"
+              value="light"
+              title="light"
+              {...register("theme")}
+            />
+            <span className="block">Preferência Light (theme)</span>
+          </label>
+
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <input
+              type="radio"
+              value="dark"
+              title="dark"
+              {...register("theme")}
+            />
+            <span className="block">Preferência Dark (theme)</span>
+          </label>
+
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Título (title) (Obrigatório)</span>
+            <textarea
+              id="title"
+              title="title"
+              className="border border-black"
+              {...register("title")}
+            />
+          </label>
+          <label className="block border border-black py-2 px-2 bg-gray-100">
+            <span className="block">Descrição (description) (Opcional)</span>
+            <textarea
+              id="description"
+              title="description"
+              className="border border-black"
+              {...register("description")}
+            />
           </label>
           <button
             type="submit"
---
+++ my-app/src/schemas/formAddUserSchema.ts
@@ -0,0 +1,50 @@
+import { SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+
+// schema
+export const formSchema = z.object({
+  // input
+  name: z.string().min(1, "Name should be longer than 1 character"),
+  // email
+  email: z
+    .string()
+    .email("Invalid email address")
+    .min(1, "email should be longer than 1 character"),
+  // money
+  salary: z.string().min(1, "Salary should be longer than 1 character"),
+  // percentage
+  raise: z.string().min(1, "Raise should be longer than 1 character"),
+  // cep
+  cep: z.string().min(1, "CEP should be longer than 1 character"),
+  // cpf
+  cpf: z.string().min(1, "Name should be longer than 1 character"),
+  // int
+  workdays: z.string().min(1, "Work Days should be longer than 1 character"),
+  // password and confirm
+  password: z.string().min(1, "Password should be longer than 1 character"),
+  repassword: z.string().min(1, "Repassword should be longer than 1 character"),
+  // date
+  birthday: z.string().min(1, "Birthday should be longer than 1 character"),
+  // number
+  age: z.number().int().min(1, "Age should be longer than 1 character"),
+  // checkbox
+  agree: z
+    .boolean()
+    .refine((value) => value === true, { message: "You must accept the term" }),
+  // select
+  genre: z.string().nonempty("Select an option"),
+  // radio
+  theme: z.enum(["light", "dark"]),
+  // textarea
+  title: z.string().min(5, "Minimum 5 characters"),
+  // textarea optional
+  description: z.string().optional(),
+});
+
+// type
+export type TFormValues = z.infer<typeof formSchema>;
+
+// submit
+export const onSubmitAddUser: SubmitHandler<TFormValues> = (data) => {
+  console.log("data submitted", data);
+};
